a367ecf92bff9f7a351387831c1a8bd151059083
Antra-Verma <51118723+Antra-Verma@users.noreply.github.com>
2020-09-13 10:10:19 +0530

    Updates

---
+++ precache-manifest.a536836aa4dc8008867eca4c7b1e4096.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c234deaa523a71664b50cc419dc2e369",
+    "url": "/To-do-app/index.html"
+  },
+  {
+    "revision": "21e8f7d1d5e5d7af5ad0",
+    "url": "/To-do-app/static/css/main.f8d773dc.chunk.css"
+  },
+  {
+    "revision": "10e70d2828e7611b6538",
+    "url": "/To-do-app/static/js/2.fb4bf519.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-do-app/static/js/2.fb4bf519.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21e8f7d1d5e5d7af5ad0",
+    "url": "/To-do-app/static/js/main.2dd8f146.chunk.js"
+  },
+  {
+    "revision": "f348716182a4d041f353",
+    "url": "/To-do-app/static/js/runtime-main.30df6c5b.js"
+  }
+]);
